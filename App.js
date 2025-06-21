f4f71780465bec4c627156c0dc3786ec3ddfc342
bharath1818v <bharathittireddy18@gmail.com>
2025-06-22 00:58:36 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center' }}>
+      <h1>Welcome to B18 Market 🛍️</h1>
+      <p>You can sell or buy anything in 18 minutes on B18 Market 🚀</p>
+    </div>
+  );
+}
+
+export default App;
